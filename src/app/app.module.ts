46dfde53c07808255b947a9d226b67c465f0fda9
unknown <rvechiato@gmail.com>
2018-02-23 15:43:21 -0300

    body-main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
+import { BodyMainComponent } from './body-main/body-main.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    FooterComponent
+    FooterComponent,
+    BodyMainComponent
   ],
   imports: [
     BrowserModule
